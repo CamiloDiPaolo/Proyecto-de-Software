f84ed76f53c2e15c0ee640dab4c468e5d168389a
Joaquin Fontana <joaquinfontana11@gmail.com>
2022-10-08 16:00:30 -0300

    Cambio de credenciales

--- src/core/db.py
+++ src/core/db.py
@@ -6,7 +6,7 @@ from sqlalchemy import Integer
 from sqlalchemy import String
 
 
-engine = create_engine(f"postgresql+psycopg2://admin:admin@localhost:5432/proyectoSoftware", echo=True, future=True)
+engine = create_engine(f"postgresql+psycopg2://grupo21:DU33zke7qqZooYKlBDZH@https://admin-grupo21.proyecto2022.linti.unlp.edu.ar/", echo=True, future=True)
 
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
