dcd525a8b0bd901c11312cd50c5f912389aed34e
Facundo Odone <facuodone1@gmail.com>
2022-10-17 23:42:31 -0300

    Buscador y paginacion implementados

--- admin/src/web/controllers/perAsoc.py
+++ admin/src/web/controllers/perAsoc.py
@@ -1,10 +1,10 @@
 from flask import Blueprint,render_template,redirect,request,jsonify
-import json,datetime,random
+import json,datetime,random,ast,math
 from src.core.db import db_session
 from src.core.models.Socio import Socio
+from src.core.models.Configuracion import Configuracion
 from src.web.controllers.FactoryCrud import create_doc_json, delete_doc_json, get_all_docs_json, get_doc_json, get_all_docs_paginated_json
 
-import ast
 
 
 perAsoc_blueprint = Blueprint('perAsoc_blueprint', __name__, url_prefix='/socios')
@@ -85,62 +85,86 @@ def deleteSoc(id):
     return redirect("/socios")
 
 
-@perAsoc_blueprint.route("/find", methods=["POST","GET"])
-def buscador():
-    socios_dict=request.form.to_dict()
-    
-    print("ESTO ME LLEGO POR EL FORMULARIO ")
-    print (socios_dict)
-    
-    socios=[]
-    
-    
-    if (socios_dict["apellido"] != ''):
-        result=db_session.query(Socio).filter_by(apellido=socios_dict["apellido"]).all()
-    
-    #if (socios_dict["estado"]=="activo"):
-    #   result = db_session.query(Socio).filter_by(estado = True).all()
-    #elif (socios_dict["estado"]=="inactivo"):
-    #    result = db_session.query(Socio).filter_by(estado = False).all()
-    #else:
-    #    return redirect("/socios")
+@perAsoc_blueprint.route("/find/<tipo>/<value>/<page>", methods=["POST","GET"])
+def buscador(page,tipo,value):
+    socios_dict={"estado":tipo, "apellido":value}
+    result=[] 
+    if (socios_dict["apellido"] != 'vacio'):
+        #if (socios_dict["estado"] == 'Activo'):
+        if (socios_dict["estado"]=='nada'):
+            result=get_all_partners_paginated_filter_json(page, socios_dict["apellido"], "apellido")
         
-    for row in result:
-        socios.append(row.json)
+        elif (socios_dict["estado"]=='activo'):
+            socios_dict["estado"]=True
+            result=get_all_partners_paginated_filter_json(page, True, socios_dict)
+            
+        elif (socios_dict["estado"]=='inactivo'):
+            socios_dict["estado"]=False
+            result=get_all_partners_paginated_filter_json(page, False, socios_dict)
         
-    print("ANTES DE RENDERIZAR, IMPRIMO")
-    print(socios)
-    return render_template("index_perAsoc.html", socio=socios)
+    elif (socios_dict["estado"] != 'nada'):
+        
+        if (socios_dict["estado"]=='activo'):
+            result=get_all_partners_paginated_filter_json(page, True, "estado")
+            
+        elif (socios_dict["estado"]=='inactivo'):
+            result=get_all_partners_paginated_filter_json(page, False, "estado")
+            
+    elif ((socios_dict["apellido"]=='vacio') & (socios_dict["estado"]=='nada')):
+        return redirect ("/socios/paginado/0")
+
+    return render_template("index_perAsoc.html", socio=result["docs"], max_page = result["total_pages"], tipo=tipo, value=value)
     
     
 @perAsoc_blueprint.route("/descargarPDF/<lista>", methods=["GET"])
 def descargarPDF(lista):
-    print("EL TIPO DE PARAMETRO ES ")
-    print(type(lista))
-    return render_template("prueba.html", data=lista)
+    lista_dict=ast.literal_eval(json.dumps(lista))
+    #print(type(lista_dict))
+    return render_template("prueba.html", data=lista_dict)
     
     
-@perAsoc_blueprint.route("/descargarCSV/<lista>")
+@perAsoc_blueprint.route("/paginado/descargarCSV/<lista>")
 def descargarCSV(lista):
-    print (ast.literal_eval(lista))
     return render_template("prueba.html", data=lista)
     
 
 def get_all_partners_paginated_filter_json(page, value, tipo):
     config = get_doc_json(Configuracion, 1)
     rows_per_page = config["elementos_por_pag"]
+    all_pages = 1
+    result = []
 
     json = []
     if(tipo == "nombre"):
         result = db_session.query(Socio).filter(Socio.nombre.ilike("%" + value + "%")).limit(rows_per_page).offset(int(page)*rows_per_page)
-    else:
+        len_result = db_session.query(Socio).filter(Socio.nombre.ilike("%" + value + "%")).all()
+        all_pages = math.ceil(len(len_result) / rows_per_page)
+
+        
+    elif (tipo =="apellido"):
+        result = db_session.query(Socio).filter(Socio.apellido.ilike("%" + value + "%")).limit(rows_per_page).offset(int(page)*rows_per_page)
+        len_result = db_session.query(Socio).filter(Socio.apellido.ilike("%" + value + "%")).all()
+        all_pages = math.ceil(len(len_result) / rows_per_page)
+         
+    elif (tipo == "estado"):
+        result = db_session.query(Socio).filter_by(estado = value).limit(rows_per_page).offset(int(page)*rows_per_page)
+        len_result = db_session.query(Socio).filter_by(estado = value).all()
+        all_pages = math.ceil(len(len_result) / rows_per_page)
+        
+    elif (tipo =="nro_socio"):
         result = db_session.query(Socio).filter(Socio.nro_socio == value).limit(rows_per_page).offset(int(page)*rows_per_page)
+        len_result = db_session.query(Socio).filter(Socio.nro_socio == value).all()
+        all_pages = math.ceil(len(len_result) / rows_per_page)
+
         # TODO: Implementar el ILIKE pero con numeros
         # result = db_session.query(Socio).filter(Socio.nro_socio.ilike("%" + value + "%")).limit(rows_per_page).offset(int(page)*rows_per_page)
+    else:
+        result = db_session.query(Socio).filter_by(estado = tipo["estado"]).filter(Socio.apellido.ilike("%" + tipo["apellido"]+ "%")).limit(rows_per_page).offset(int(page)*rows_per_page)
+        len_result = db_session.query(Socio).filter_by(estado = tipo["estado"]).filter(Socio.apellido.ilike("%" + tipo["apellido"]+ "%")).all()
+        all_pages = math.ceil(len(len_result) / rows_per_page)
+        
     for row in result:
         json.append(row.json())
 
-    result = db_session.query(Socio).all()
-    all_pages = math.floor(len(result) / rows_per_page) if math.floor(len(result) / rows_per_page) > 0 else 1
     return {"docs": json, "total_pages": all_pages}
 
