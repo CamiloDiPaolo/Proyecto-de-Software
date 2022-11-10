4e33211aaa1a70f8ac6855c10b0fc9b7f42e50b2
Facundo Odone <facuodone1@gmail.com>
2022-11-10 17:36:04 -0300

    Ahora si descarga

--- admin/src/web/controllers/CSVCreate.py
+++ admin/src/web/controllers/CSVCreate.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import json
-from flask import make_response,jsonify, request
+from flask import make_response,jsonify, request,send_file
 
 def createCSV(result):
 
@@ -8,6 +8,6 @@ def createCSV(result):
     df= pd.DataFrame(result) #columns=('nro_socio','email','nombre','apellido','tipo_documento','nro_documento'))
     df.to_csv('listado.csv', index=False, header=False, encoding='utf-8')
     #response = make_response(df)
-    #response.headers.set('Content Type', 'text/csv')
+    #response.headers.set('Content Type', 'aplicattion/csv')
     #response.headers.set("Content-Disposition","attachment",filename="listado.csv")
-    return ('<h1> descargado </h1>')
+    return send_file(df,download_name='reporte.csv',mimetype='text/csv',as_attachment=True)
