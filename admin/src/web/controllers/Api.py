189d3fb8b7b9d18b2f12cbc88c264f39feebdc44
Facundo Odone <facuodone1@gmail.com>
2022-11-17 16:57:40 -0300

    seguimos en la misma

--- admin/src/web/controllers/Api.py
+++ admin/src/web/controllers/Api.py
@@ -294,7 +294,7 @@ def paymentExist(payment,id):
 
 def cors(res):
     #res.headers.add("Access-Control-Allow-Origin", "http://localhost:5173") #USO LOCAL
-    res.headers.add("Access-Control-Allow-Origin", "https://grupo21.proyecto2022.linti.unlp.edu.ar") #USO REMOTO
+    #res.headers.add("Access-Control-Allow-Origin", "https://grupo21.proyecto2022.linti.unlp.edu.ar") #USO REMOTO
     res.headers.add("Access-Control-Allow-Headers", "X-Requested-With,content-type")
     res.headers.add("Access-Control-Allow-Methods", "*")
     res.headers.add("Access-Control-Allow-Credentials", "true")
